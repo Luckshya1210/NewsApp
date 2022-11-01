c6fd0634f5d1a15dd0973087136f0f71f7e173ca
Luckshya <luckshya1210@gmail.com>
2022-11-01 23:02:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component, useState } from 'react'
+import Navbar from './components/Navbar';
+// import { useState } from 'react';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
+import LoadingBar from 'react-top-loading-bar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+const App=()=> {
+  //class use this c='luck';
+  const pageSize=15;
+ const [progress,setprogress]=useState(0)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <Router>
+          
+          <Navbar/>
+          <LoadingBar
+        color='#f11946'
+        progress={progress}
+        height={3}
+      />
+         <Routes>       
+          <Route exact path="/" element={<News setprogress={setprogress}  key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
+          <Route exact path="/business" element={<News setprogress={setprogress}  key="business" pageSize={pageSize} country="in" category="business"/>}></Route>
+          <Route exact path="/entertainment" element={<News setprogress={setprogress}  key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}></Route>
+          <Route exact path="/general" element={<News setprogress={setprogress}  key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
+          <Route exact path="/health" element={<News setprogress={setprogress}  key="health" pageSize={pageSize} country="in" category="health"/>}></Route>
+          <Route exact path="/science" element={<News setprogress={setprogress}  key="science" pageSize={pageSize} country="in" category="science"/>}></Route>
+          <Route exact path="/sports" element={<News setprogress={setprogress}  key="sports" pageSize={pageSize} country="in" category="sports"/>}></Route>
+          <Route exact path="/technology" element={<News setprogress={setprogress}  key="technology" pageSize={pageSize} country="in" category="technology"/>}></Route>
+          
+        </Routes>
+          </Router>
+      </div>
+    )
+  
 }
-
-export default App;
+export default App
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Spinnerr from './Spinnerr.gif'
+ const Spinner =()=>{
+  
+    return (
+      <div className='text-center'>
+        <img src={Spinnerr} alt="loading" className='my-3'/>
+      </div>
+    )
+  
+}
+export default Spinner
